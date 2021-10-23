8a776e6bf0a4456e855a51acbf0a36759b00a6fa
kagayki <86405899+KapaBlanka212@users.noreply.github.com>
2021-10-23 18:07:17 +0300

    Create def MMT
    In MMT create def MMT:
    create array with T and R

--- MMT.py
+++ MMT.py
@@ -1,112 +1,91 @@
 # Cod for transfer matrix method
 # TODO: realisation matrix P and D : DONE
+import time
 import MaterialConstant
-import cmath
-import sympy as sp
 import numpy as np
-from sympy import Symbol, simplify
-from cmath import exp
-from numpy import *
+from numpy import exp,matrix,array,real,imag,sqrt,pi,transpose
 #==========================================================#
 #                     CONSTANT                             #
 #==========================================================#
 m0 = 9.1*10**(-31) # electron mass kg
 me = 0.35 * m0 # electron mass in thin membrane
+e0 = 1.6*10**(-19) # Kulon
 const = MaterialConstant.MaterialConstant()
-c = const.lightspeed
-d_K108 = const.d_K108
-l = const.full_wawelenght*10**(-2)
-w = 2*pi*c*(const.full_wawelenght*(10**(-2))) # sec^(-1)
-#print(l.shape)
-n_K108 = const.n_K108
-k_K108 = (const.alfa_K108)*(l/(4*pi))
-n_vac = const.n_vac
+c = const.lightspeed*10**(-2) # cm/c
+d_K108 = const.d_K108 #  cm
+l = const.full_wawelenght # cm
+w = 2*pi*c*(l) # sec^(-1)
+n_K108 = const.n_K108 # K108
+k_K108 = (const.alfa_K108)*(l/(4*pi)) # K108
+n_vac = const.n_vac # VACUUM
 k_vac = const.alfa_vac # 0 == 0
-i = 1j # im 1
+i = 1j # Im 1
+indx = const.indx
+#==========================================================#
+#                   CHANGES CONSTANT                       #
+#==========================================================#
+dm = 4*10**(-5) # m # 5*10**(-5)
+Ne =  10 ** (20) # cm^-3 # 10 ** (21)
+t = 10**(14) # 1/tau # 10*(15)
+eps_Inf = 3 # 5
 #==========================================================#
 #                     THEORY DRUDE                         #
 #==========================================================#
-def eps(eps_inf,t):
-    def w_p(Ne):
-        w_p_return = math.sqrt((4*pi*Ne)/(eps_inf*me))
-        return w_p_return
-    eps_return = eps_inf * (1.0 - ((w_p**(2))/(w(w+(1j/t)))))
+def eps(eps_inf,w_p,t,w): # t = 1/tau
+    eps_return = eps_inf * (1.0 - ((w_p**(2))/(w*(w+(i*t)))))
     return eps_return
+def w_p(Ne,eps_inf):
+    w_p_out = np.sqrt((4*pi*Ne*e0**(2))/(eps_inf*me))
+    return w_p_out
 def n_m(eps):
-    n_m_return = number.real(eps)
+    n_m_return = real(np.sqrt(eps))
     return n_m_return
 def k_m(eps):
-    k_m_return = number.imag(eps)
+    k_m_return = imag(np.sqrt(eps))
     return k_m_return
 #==========================================================#
 #     Complex refractive index and electric potential      #
 #==========================================================#
 # TODO: realisation MMT for only one point l/w : DONE
 def n_(n,k):
-    n_return = n+(1j)*k
+    n_return = n+(i)*k
     return n_return
 def fi(w,n_,d):
     fi_out = (w/c)*n_*d
     return fi_out
 #==========================================================#
-#                  D and P matrix                          #
-#==========================================================#
-nm = Symbol('n')
-km = Symbol('k')
-dm = Symbol('d')
-n_m = nm +i*km
-# for all point (problem with symbols math)
-# 0-1
-#D0 = (1/2)*(1/n_(nm,km))*sp.Matrix([[(n_(nm,km)+n_vac),*(n_(nm,km)-n_vac)],
-#                                    [(n_(nm,km)-n_vac),*(n_(nm,km)+n_vac)]])
-# 1
-#P_m = sp.Matrix([[sp.exp(1j*fi(n_(nm,km),dm)),0],
-#                [0,sp.exp(-1j*fi(n_(nm,km),dm))]])
-# 1-2
-#D1 = (1/(2*n_(n_K108,k_K108)))*sp.Matrix[[*(n_(nm,km)+n_(n_K108,k_K108)),(n_(n_K108,k_K108)-n_(nm,km))],
-#                                         [(n_(n_K108,k_K108)-n_(nm,km)),(n_(nm,km)+n_(n_K108,k_K108))]])
-# 2
-#P_K108 = np.matrix([[np.exp(1j*fi(n_(n_K108,k_K108),d_K108)),0],
-#                   [0,np.exp(-1j*fi(n_(n_K108,k_K108),d_K108))]])
-# 2-3
-#D2 = (1/(2*n_vac))*sp.Matrix([[(n_vac+n_(n_K108,k_K108)),(n_vac - n_(n_K108,k_K108))],
-#                             [(n_vac-n_(n_K108,k_K108)),(n_vac+n_(n_K108,k_K108))]])
-
-# for one point
-
-# 0-1
-D0 = (1/2)*(1/n_m)*sp.Matrix([[n_m + n_vac[0],n_m - n_vac[0]],
-                              [n_m - n_vac[0],n_m + n_vac[0]]])
-# 1
-P_m = sp.Matrix([[sp.exp(i*fi(w[0],n_(nm,km),dm)),0],
-                [0,sp.exp(-i*fi(w[0],n_(nm,km),dm))]])
-# 1-2
-D1 = (1/(2*n_(n_K108[0],k_K108[0])))*sp.Matrix([[n_m+n_(n_K108[0],k_K108[0]),n_(n_K108[0],k_K108[0])-n_m],
-                                               [n_(n_K108[0],k_K108[0])-n_m,n_m+n_(n_K108[0],k_K108[0])]])
-# 2
-P_K108 = sp.Matrix([[cmath.exp(i*fi(w[0],n_(n_K108[0],k_K108[0]),d_K108)),0],
-                   [0,cmath.exp(-i*fi(w[0],n_(n_K108[0],k_K108[0]),d_K108))]])
-# 2-3
-D2 = (1/(2*n_vac[0]))*sp.Matrix([[n_vac[0]+n_(n_K108[0],k_K108[0]),n_vac[0] - n_(n_K108[0],k_K108[0])],
-                                 [(n_vac[0]-n_(n_K108[0],k_K108[0])),n_vac[0]+n_(n_K108[0],k_K108[0])]])
-#print(D0)
-#print('')
-#print(P_m)
-#print('')
-#print(D1)
-#print('')
-#print(P_K108)
-#print('')
-#print(D2)
-#print('')
-
-#==========================================================#
 #                  CALCULATING T AND R                     #
 #==========================================================#
-M = D2@P_K108@D1@P_m@D0
-T = abs(M[0,0]-(M[0,1]*M[1,0])/M[1,1])
-T = simplify(T)
-R = abs(M[1,0]/M[1,1])**(2)
-R = simplify(R)
-print('T ==',T)
-print('R ==',R)
+start1 = time.time()
+wp = w_p(Ne, eps_Inf) # omega plasmone
+Tm = [] # list of T
+Rm = [] # list of R
+def MMT(x):
+    for m in range(0,x):
+        eps_ = eps(eps_Inf, wp, t, w[m])
+        nm = n_m(eps_)
+        km = k_m(eps_)
+        D0 = (1 / 2) * (1 / nm) * np.matrix([[nm + n_vac[m], nm - n_vac[m]],
+                                             [nm - n_vac[m], nm + n_vac[m]]])
+        # 1
+        P_m = np.matrix([[np.exp(i * fi(w[m], n_(nm, km), dm)), 0],
+                         [0, np.exp(-i * fi(w[m], n_(nm, km), dm))]])
+        # 1-2
+        D1 = (1 / (2 * n_(n_K108[m], k_K108[m]))) * np.matrix([[nm + n_(n_K108[m], k_K108[m]), n_(n_K108[m], k_K108[m]) - nm],
+                                                           [n_(n_K108[m], k_K108[m]) - nm, nm + n_(n_K108[m], k_K108[m])]])
+        # 2
+        P_K108 = np.matrix([[np.exp(i * fi(w[m], n_(n_K108[m], k_K108[m]), d_K108)), 0],
+                        [0, np.exp(-i * fi(w[m], n_(n_K108[m], k_K108[m]), d_K108))]])
+        # 2-3
+        D2 = (1 / (2 * n_vac[m])) * np.matrix([[n_vac[m] + n_(n_K108[m], k_K108[m]), n_vac[m] - n_(n_K108[m], k_K108[m])],
+                                           [(n_vac[m] - n_(n_K108[m], k_K108[m])),n_vac[m] + n_(n_K108[m], k_K108[m])]])
+        M = D2 @ P_K108 @ D1 @ P_m @ D0
+        T = abs(M[0,0]-(M[0,1]*M[1,0])/M[1,1])
+        Tm.append(T)
+        R = abs(M[1,0]/M[1,1])**(2)
+        Rm.append(R)
+    anser = np.array([[Tm],[Rm]])
+    return anser
+print(MMT(indx))
+end1 = time.time()
+print(end1 - start1)
