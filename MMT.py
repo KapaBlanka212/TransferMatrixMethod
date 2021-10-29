195f792b4b55d8d9387b4403b24070755a254b2e
kagayki <86405899+KapaBlanka212@users.noreply.github.com>
2021-10-29 20:05:07 +0300

    P_K108

--- MMT.py
+++ MMT.py
@@ -1,14 +1,13 @@
-# Cod for transfer matrix method
-# TODO: realisation matrix P and D : DONE
-import random
 import time
 import MaterialConstant
 import numpy as np
 from numpy import *
 import scipy as sp
+from sympy import Symbol
 from scipy.optimize import minimize
+import scipy.integrate as integrate
 # ==========================================================#
-#                     CONSTANT                             #
+#                      CONSTANT                             #
 # ==========================================================#
 m0 = 9.1 * 10 ** (-31)  # electron mass kg
 me = 0.35 * m0  # electron mass in thin membrane
@@ -17,7 +16,7 @@ const = MaterialConstant.MaterialConstant()
 c = const.lightspeed * 10 ** (-2)  # cm/c
 d_K108 = const.d_K108  # cm
 l = const.full_wavelenght  # cm
-w = 2 * pi * c * (l)  # sec^(-1)
+w = 2 * pi * c / (l)  # sec^(-1)
 n_K108 = const.n_K108  # K108
 k_K108 = (const.alfa_K108) * (l / (4 * pi))  # K108
 n_vac = const.n_vac  # VACUUM
@@ -34,7 +33,7 @@ full_exp = np.vstack([T_exp, R_exp])
 # ==========================================================#
 #           ZERO APPROXIMATION OF PARAMETERS               #
 # ==========================================================#
-d_0 = 400 * 10 ** (-5)  # m # 500*10**(-5)
+d_0 = 400 * 10 ** (-5)  # sm # 500*10**(-5)
 Ne_0 = 10 ** (20)  # cm^-3 # 10 ** (21)
 t_0 = 10 ** (14)  # 1/tau # 10*(15)
 eps_Inf_0 = 3.0  # 5
@@ -79,6 +78,11 @@ def fi(w, n_, d):
     return fi_out
 
 
+def fi_(w, n_, d, delta):
+    fi_out = (w / c) * n_ * d + delta/2
+    return fi_out
+
+
 # ==========================================================#
 #                  CALCULATING T AND R                     #
 # ==========================================================#
@@ -91,7 +95,7 @@ x = np.array([Ne_f,eps_Inf_f,t_f,d_f]).transpose()
 x0 = np.array([Ne_0,eps_Inf_0,t_0,d_0]).transpose()
 
 
-def mmt(x):
+def mmt(x,delta):
     z = indx2
     Tm = []  # list of T
     Rm = []  # list of R
@@ -109,36 +113,40 @@ def mmt(x):
         D1 = (1 / (2 * n_(n_K108[m], k_K108[m]))) * np.matrix([[nm + n_(n_K108[m], k_K108[m]), n_(n_K108[m], k_K108[m]) - nm],
                                                                 [n_(n_K108[m], k_K108[m]) - nm, nm + n_(n_K108[m], k_K108[m])]])
         # 2
-        P_K108 = np.array([[np.exp(i * fi(w[m], n_(n_K108[m], k_K108[m]), d_K108)), 0],
-                           [0, np.exp(-i * fi(w[m], n_(n_K108[m], k_K108[m]), d_K108))]])
+        P_K108 = np.array([[(np.exp(i * fi_(w[m], n_(n_K108[m], k_K108[m]), d_K108, delta))), 0],
+                            [0, np.exp(-i * fi_(w[m], n_(n_K108[m], k_K108[m]), d_K108, delta))]])
         # 2-3
         D2 = (1 / (2 * n_vac[m])) * np.array([[n_vac[m] + n_(n_K108[m], k_K108[m]), n_vac[m] - n_(n_K108[m], k_K108[m])],
                                             [(n_vac[m] - n_(n_K108[m], k_K108[m])), n_vac[m] + n_(n_K108[m], k_K108[m])]])
         M = D2 @ P_K108 @ D1 @ P_m @ D0
-        T = abs(M[0, 0] - (M[0, 1] * M[1, 0]) / M[1, 1])
+        T = (1/(2*pi))*integrate.quad(lambda : delta, abs(M[0, 0] - (M[0, 1] * M[1, 0]) / M[1, 1]) ** (2), 0, 2*pi)
         Tm.append(T)
-        R = abs(M[1, 0] / M[1, 1]) ** (2)
+        R = (1/(2*pi))*integrate.quad(lambda: delta, abs(M[1, 0] / M[1, 1]) ** (2),0, 2*pi)
         Rm.append(R)
     T1 = np.array(Tm)
     R1 = np.array(Rm)
     eqn1 = np.vstack([[T1], [R1]])
     return eqn1
-    #print (mmt(Ne_0,eps_Inf_0,t_0,d_0))
+
 
 # ==========================================================#
-#                  FIND OPTIMAL PARAMETERS                 #
-# ==========================================================#w
-# TODO: find optimal parameters d,Ne,t,eps_Inf
+#                   FIND OPTIMAL PARAMETERS                 #
+# ==========================================================#
+# TODO: find optimal parameters d,Ne,t,eps_Inf : in progress
 
 
+def func(x,delta):
+    S = mmt(x,delta) - full_exp
+    S1 = np.std(S)
+    return S1
 
-def func(x):
-    S = mmt(x) - full_exp
-    S_T_0 = np.std(S[0, :])
-    return S_T_0
-
-bnds = ((Ne_0,10**21),(eps_Inf_0,5),(t_0,10**14),(d_0,500*10**(-5)))
-ans = sp.optimize.minimize(func,x0,method = 'Nelder-Mead',bounds=bnds,options={'disp':True,'return_all': True})
+delta = lambda : delta
+bnds = ((Ne_0/10,None),(eps_Inf_0/300,None),(t_0/10,None),(d_0/10,None))
+ans = sp.optimize.minimize(func,x0,method = 'Nelder-Mead',args = delta,
+                           bounds=bnds,options={'disp':True,'return_all': True})
 print(ans)
+print(mmt((ans.x)),l*1000)
+#x1 = np.array([1.17055461e+20, 1.30003878e+00, 1.23917067e+14, 4.30471629e-03])
+#print([[mmt(x)],[l*1000]])
 end1 = time.time()
 print(end1 - start1)
